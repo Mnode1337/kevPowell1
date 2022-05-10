f1521f22cbfd7b1a7d2dbee8d0dbe5ffa6505c8d
Master Node <franck.komla@gmail.com>
2022-05-10 19:03:25 +0000

    navbar and hamburger

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+const navToggle = document.querySelector('.nav-toggle');
+
+const navLinks = document.querySelectorAll('.nav__link');
+
+
+
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+
+
+navLinks.forEach(element => {
+
+    element.addEventListener('click', () => {
+
+        document.body.classList.remove('nav-open');
+
+    })
+
+});
